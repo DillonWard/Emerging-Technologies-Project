a752530ed966f6746141af6002790eb2aac71813
Dillon Ward <Dillonward13@gmail.com>
2017-11-13 12:30:01 +0000

    uploading image to page

--- static/js/controller.js
+++ static/js/controller.js
@@ -1,19 +1,16 @@
 // Adapted from - https://stackoverflow.com/questions/12368910/html-display-image-after-selecting-filename
 
-$("input").change(function(e) {
-    
-        for (var i = 0; i < e.originalEvent.srcElement.files.length; i++) {
-            
-            var file = e.originalEvent.srcElement.files[i];
-            
-            var img = document.createElement("img");
-            var reader = new FileReader();
-            
-            reader.onloadend = function() {
-                 img.src = reader.result;
-            }
-            
-            reader.readAsDataURL(file);
-            $("input").after(img);
-        }
-    });
+function uploadImage(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#img')
+                .attr('src', e.target.result)
+                .width(150)
+                .height(200);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
