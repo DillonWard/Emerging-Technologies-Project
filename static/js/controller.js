70ee777c6dec0c37c2248a164ed4d16792214d07
Dillon <Dillonward13@gmail.com>
2017-11-14 17:54:59 +0000

    working on tabs for chaning views

--- static/js/controller.js
+++ static/js/controller.js
@@ -1,5 +1,6 @@
-// Adapted from - https://stackoverflow.com/questions/12368910/html-display-image-after-selecting-filename
+// (1) Adapted from - https://stackoverflow.com/questions/12368910/html-display-image-after-selecting-filename
 
+// Function for selecting and uploading a file to the web page - (1)
 function uploadImage(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
@@ -10,7 +11,27 @@ function uploadImage(input) {
                 .width(150)
                 .height(200);
         };
-
         reader.readAsDataURL(input.files[0]);
     }
 }
+
+// function openTab(evt, tabName) {
+//     // Declare all variables
+//     var i, tabcontent, tablinks;
+
+//     // Get all elements with class="tabcontent" and hide them
+//     tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i = 0; i < tabcontent.length; i++) {
+//         tabcontent[i].style.display = "none";
+//     }
+
+//     // Get all elements with class="tablinks" and remove the class "active"
+//     tablinks = document.getElementsByClassName("tablinks");
+//     for (i = 0; i < tablinks.length; i++) {
+//         tablinks[i].className = tablinks[i].className.replace(" active", "");
+//     }
+
+//     // Show the current tab, and add an "active" class to the button that opened the tab
+//     document.getElementById(tabName).style.display = "block";
+//     evt.currentTarget.className += " active";
+// }
