dde91963510371414d287ef1b3c553a3bc9caf97
Dillon Ward <Dillonward13@gmail.com>
2017-11-27 14:18:35 +0000

    can draw on and clear canvas

--- static/js/controller.js
+++ static/js/controller.js
@@ -1,6 +1,7 @@
 // (1) Adapted from - https://stackoverflow.com/questions/12368910/html-display-image-after-selecting-filename
 // (2) Adapted from - https://www.w3schools.com/howto/howto_js_tab_header.asp
-  
+// (3) Adapted from - https://stackoverflow.com/questions/2368784/draw-on-html5-canvas-using-a-mouse
+
 // Function for selecting and uploading a file to the web page - (1)
 function uploadImage(input) {
     if (input.files && input.files[0]) {
@@ -40,3 +41,83 @@ function openTab(tabName, elmnt, color) {
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+// Function for creating a canvas and allowing the user to draw/erase
+var canvas, ctx, flag = false,
+prevX = 0,
+currX = 0,
+prevY = 0,
+currY = 0,
+dot_flag = false;
+
+var x = "black",
+y = 2;
+
+function init() {
+canvas = document.getElementById('can');
+ctx = canvas.getContext("2d");
+w = canvas.width;
+h = canvas.height;
+
+canvas.addEventListener("mousemove", function(e) {
+    findxy('move', e)
+}, false);
+canvas.addEventListener("mousedown", function(e) {
+    findxy('down', e)
+}, false);
+canvas.addEventListener("mouseup", function(e) {
+    findxy('up', e)
+}, false);
+canvas.addEventListener("mouseout", function(e) {
+    findxy('out', e)
+}, false);
+}
+
+function draw() {
+ctx.beginPath();
+ctx.moveTo(prevX, prevY);
+ctx.lineTo(currX, currY);
+ctx.strokeStyle = x;
+ctx.lineWidth = y;
+ctx.stroke();
+ctx.closePath();
+}
+
+function erase() {
+var m = confirm("Want to clear");
+if (m) {
+    ctx.clearRect(0, 0, w, h);
+    document.getElementById("canvasimg").style.display = "none";
+}
+}
+
+function findxy(res, e) {
+if (res == 'down') {
+    prevX = currX;
+    prevY = currY;
+    currX = e.clientX - canvas.offsetLeft - 235;
+    currY = e.clientY - canvas.offsetTop - 28;
+
+    flag = true;
+    dot_flag = true;
+    if (dot_flag) {
+        ctx.beginPath();
+        ctx.fillStyle = x;
+        ctx.fillRect(currX, currY, 2, 2);
+        ctx.closePath();
+        dot_flag = false;
+    }
+}
+if (res == 'up' || res == "out") {
+    flag = false;
+}
+if (res == 'move') {
+    if (flag) {
+        prevX = currX;
+        prevY = currY;
+        currX = e.clientX - canvas.offsetLeft - 235;
+        currY = e.clientY - canvas.offsetTop - 28;
+        draw();
+    }
+}
+}
